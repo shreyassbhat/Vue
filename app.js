551a98a999d37dab20640c86e0a7befcac65ca90
Shreyas S Bhat <35568964+shreyassbhat@users.noreply.github.com>
2018-01-21 17:01:41 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,26 @@
+new Vue({
+  el:'#vue_app',
+ data:{
+   health:100,
+   ended:false
+
+
+  },
+  methods:{
+    punch:function(){
+      this.health -=10;
+      if(this.health<=0){
+        this.ended=true;
+      }
+    },
+    restart:function(){
+      this.health=100;
+      this.ended=false;
+    }
+
+  },
+  computed:{
+
+  }
+
+});
